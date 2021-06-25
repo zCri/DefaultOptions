2903c9d4e1cd779fbc5c1cd29bfa68a04015f63a
zCri <40072654+zCri@users.noreply.github.com>
2021-06-25 23:30:48 +0200

    Update DefaultOptions.java

--- src/main/java/net/blay09/mods/defaultoptions/DefaultOptions.java
+++ src/main/java/net/blay09/mods/defaultoptions/DefaultOptions.java
@@ -369,7 +369,7 @@ public class DefaultOptions {
             if (defaultKeys.containsKey(keyBinding.getKeyDescription())) {
                 DefaultBinding defaultBinding = defaultKeys.get(keyBinding.getKeyDescription());
                 keyBinding.keyCodeDefault = defaultBinding.keyCode;
-                ReflectionHelper.setPrivateValue(KeyBinding.class, keyBinding, defaultBinding.modifier, "keyModifierDefault");
+                ReflectionHelper.setPrivateValue(KeyBinding.class, keyBinding, defaultBinding.modifier, "keyModifier");
                 if (!knownKeys.contains(keyBinding.getKeyDescription())) {
                     keyBinding.setKeyModifierAndCode(keyBinding.getKeyModifierDefault(), keyBinding.getKeyCodeDefault());
                     knownKeys.add(keyBinding.getKeyDescription());
